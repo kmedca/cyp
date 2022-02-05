1ad631d1d58352b6e550aead2101add7b3ad7631
Kevin Medlyn <kmedlyn@gofundme.com>
2022-02-05 09:35:53 -0800

    add intellisense

--- cypress/integration/sample.spec.js
+++ cypress/integration/sample.spec.js
@@ -1,5 +1,11 @@
 describe("My first test", () => {
-    it('The test does now work yet', () =>{
-        expect(true).to.equal(false)
-    })
+  it('visits a kitchen sink', () => {
+      cy.visit('https://example.cypress.io')
+      cy.contains('type').click()
+      cy.url().should('include',"/commands/actions")
+
+      cy.get('.action-email')
+        .type('fake@email')
+        .should('have.value', 'fake@email')
+      })
 })
